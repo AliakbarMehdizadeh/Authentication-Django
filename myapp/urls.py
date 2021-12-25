286917e39266ec3f3e7c2c87b61b4dff4f3e75a1
AliakbarMehdizadeh <aliakbar.mehdizadeh@gmail.com>
2021-12-25 03:33:21 +0330

    feature-1

--- myapp/urls.py
+++ myapp/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path("", views.homepage, name="homepage"),
     path("register", views.register_request, name="register"),
     path("login", views.login_request, name="login"),
+    path("question", views.get_question, name="question"),
+    path("answer", views.post_answer, name="answer"),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,43 +1,70 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect
 from .forms import NewUserForm
-from django.contrib.auth import login
+from .forms import NewQuestion
 from django.contrib import messages
-from django.contrib.auth import login, authenticate #add this
-from django.contrib.auth.forms import AuthenticationForm #add this
-
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import AuthenticationForm
+from googlesearch import search
 
 
 # Create your views here.
 def homepage(request):
-	return render(request=request, template_name='myapp/home.html')
+    return render(request=request, template_name='myapp/home.html')
+
 
 def register_request(request):
-	if request.method == "POST":
-		form = NewUserForm(request.POST)
-		if form.is_valid():
-			user = form.save()
-			login(request, user)
-			messages.success(request, "Registration successful.")
-			return redirect("myapp:homepage")
-		messages.error(request, "Unsuccessful registration. Invalid information.")
-	form = NewUserForm()
-	return render(request=request, template_name="myapp/register.html", context={"register_form": form})
+    if request.method == "POST":
+        form = NewUserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, "Registration successful.")
+            return redirect("myapp:homepage")
+        messages.error(request, "Unsuccessful registration. Invalid information.")
+    form = NewUserForm()
+    return render(request=request, template_name="myapp/register.html", context={"register_form": form})
+
 
 def login_request(request):
-	if request.method == "POST":
-		form = AuthenticationForm(request, data=request.POST)
-		if form.is_valid():
-			username = form.cleaned_data.get('username')
-			password = form.cleaned_data.get('password')
-			user = authenticate(username=username, password=password)
-			if user is not None:
-				login(request, user)
-				messages.info(request, f"You are now logged in as {username}.")
-				return redirect("myapp:homepage")
-			else:
-				messages.error(request,"Invalid username or password.")
-		else:
-			messages.error(request,"Invalid username or password.")
-	form = AuthenticationForm()
-	return render(request=request, template_name="myapp/login.html", context={"login_form":form})
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.info(request, f"You are now logged in as {username}.")
+                return redirect("myapp:homepage")
+            else:
+                messages.error(request, "Invalid username or password.")
+        else:
+            messages.error(request, "Invalid username or password.")
+    form = AuthenticationForm()
+    return render(request=request, template_name="myapp/login.html", context={"login_form": form})
 
+
+def get_question(request):
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = NewQuestion(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            return HttpResponseRedirect('/thanks/')
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = NewQuestion()
+
+    return render(request, template_name='myapp/question.html', context={'form': form})
+
+
+def post_answer(request):
+
+    res = search("Google", num_results=1)[2]
+    return render(request=request, template_name="myapp/answer.html", context={'response': res} )
